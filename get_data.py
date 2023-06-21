b7b381e825e9a9ef8cb3d68f652a1595abc75562
William <william.kang@ubc.ca>
2023-06-21 09:53:55 -0700

    Save point, correct pagination

--- get_data.py
+++ get_data.py
@@ -11,16 +11,37 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 import csv
+import os
+
+# Todo, for users -> only check based on full name?
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 def print_decorator(func):
+    # This just prints the function name before and after
     def wrapper(*args, **kwargs):
         print(f"{'-'*15}STARTING {func.__name__}{'-'*15}")
         result = func(*args, **kwargs)
         print(f"{'+'*15}FINISHED {func.__name__}{'+'*15}")
         return result
     return wrapper
+
+def append_data_to_excel(filename, df_new_data):
+    if os.path.isfile(filename):
+        try:
+            df_old = pd.read_excel(filename)
+        except Exception as e:
+            print(f"Error reading the Excel file: {e}")
+            df_combined = df_new_data
+        else:
+            df_combined = (pd.concat([df_old, df_new_data], ignore_index=True, sort =False)
+                .drop_duplicates(keep='first'))
+
+    else:
+        df_combined = df_new_data
+
+    df_combined.to_excel(filename, index=False)
+
 
 @print_decorator
 def login():
@@ -37,7 +58,8 @@ def check_page_source(driver, option):
     text = body.get_text()
     
     # Search for the pattern in the text
-    pattern = re.escape(option) + '.+'
+    # pattern = re.escape(option) + '.+'
+    pattern = re.escape(option)
     return re.search(pattern, text) is not None
 
 # Wait until the text is found in the body
@@ -55,6 +77,11 @@ def filtering():
     # List of options you want to select
     # options_to_select = ["CACE - ", "CNR - ", "CSRP - ", "CVA - ", "EFO - ", "FCM - ", "HTC - ", "FHM - ", "FSTB - ", "SMS - ", "TWS - ", "ZCBS - "]
     options_to_select = ["CVA - "] 
+    full_option_name = {"CVA - ": "CVA - Online Micro-Certificate: Climate Vulnerability & Adaptation",
+                        "CACE - ": "CACE - Online Micro-Certificate: Climate Action and Community Engagement",
+                        "CNR - ": 'CNR - Online Micro-Certificate: Co-Management of Natural Resources',
+                        "CSRP - ": "CSRP - Online Micro-Certificate: Communication Strategies for Resource Practitioners Micro-Certificate",
+                        }
 
     # Iterate over the options you want to select
     for option in options_to_select:
@@ -64,8 +91,7 @@ def filtering():
         dropdown_menu.send_keys(option)
 
         try:
-            wait.until(lambda driver: check_page_source(driver, option))
-
+            wait.until(lambda driver: check_page_source(driver, full_option_name[option]))
             # Then send the ENTER key
             catalog_filter = driver.find_element(By.CSS_SELECTOR, 'input[data-automation="AnalyticsPage__Filter__Catalog"]')
             catalog_filter.send_keys(Keys.ARROW_DOWN)
@@ -87,20 +113,33 @@ def filter_enrollment_date():
 
 def check_and_click_next_button():
     """ If the next button exists, click it and return True, else return False"""
+    # Find the span element containing the buttons
     try:
-        # Find the button with the specific class
-        next_button = driver.find_element(By.XPATH, "//button[@data-direction='next']")
+        pagination_span = driver.find_element(By.CLASS_NAME, "css-ighgvd-view--inlineBlock-pagination__pages")
 
-        # Check if the button is displayed and enabled
-        if next_button.is_displayed() and next_button.is_enabled():
-            # Press enter on the button
-            next_button.send_keys(Keys.ENTER)
+        # Find the button with aria-current="page"
+        current_button = pagination_span.find_element(By.CSS_SELECTOR,"button[aria-current='page']")
+
+        next_button = current_button.find_element(By.XPATH, "following-sibling::button")
+        if next_button:
+            print("CLICKED NEXT")
+            next_button.click()
             return True
         else:
             return False
+
     except NoSuchElementException:
         return False
         
+def convert_numeric_columns(df):
+    for column in df.columns:
+        try:
+            df[column] = pd.to_numeric(df[column], errors='raise')
+        except (ValueError, TypeError):
+            pass  # Ignore columns that cannot be converted to numeric
+
+    return df
+
 def extract_table_data(table_data):
     """ Extract aria-labels or text, assumes page has a table """
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'table')))
@@ -137,17 +176,16 @@ def extract_enrollment_table():
         table_data = extract_table_data(table_data)
         if check_and_click_next_button() == False:
             break
-    # write the data to a CSV file
-    with open('table_datafull5.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.DictWriter(csvfile, table_data[0].keys())
 
-        writer.writeheader()
-        writer.writerows(table_data)
+    # Create a DataFrame from your data
+    df = pd.DataFrame(table_data)
+    
+    df = convert_numeric_columns(df)
+    append_data_to_excel('table_datafull7.xlsx', df)
 
 
 @print_decorator
 def extract_users():
-    """ Extract users from past week (default)"""
     table_data = []
     driver.get('https://courses.cpe.ubc.ca/new_analytics/users')
     
@@ -156,17 +194,13 @@ def extract_users():
         if check_and_click_next_button() == False:
             break
 
-    with open('user_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.DictWriter(csvfile, table_data[0].keys())
+    # Create a DataFrame from your data
+    df = pd.DataFrame(table_data)
+    df = convert_numeric_columns(df)
 
-        writer.writeheader()
-        writer.writerows(table_data)
+    # Append data to Excel file
+    append_data_to_excel('user_data.xlsx', df)
     
-    df = pd.read_csv('user_data.csv', encoding='utf-8')
-    print(df)
-
-
-
 if __name__ == "__main__":
     login()
     filtering()
